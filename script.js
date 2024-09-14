54fb519a8de6c51c55f157887d91d35fc3ad091f
EduSantos20 <eduardosantosgt18@gmail.com>
2024-09-14 13:52:55 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+/**document.addEventListener("DOMContentLoaded", () => {
   const catalog = document.querySelector(".catalog")
 
   fetch("players.json")
@@ -36,14 +36,18 @@ document.addEventListener("DOMContentLoaded", () => {
         button.textContent = "Vitória"
         button.onclick = function vote() {
           const resultDiv = document.getElementById("result")
+          resultDiv.innerHTML = " "
           const resultGoolDiv = document.getElementById("resultGool")
+          resultGoolDiv.innerHTML = " "
           var gool = document.getElementById("gool").value
+          gool.innerHTML = " "
           if (gool == 0) {
             resultDiv.textContent = `Vitória do ${player.nameTime}`
           } else {
             resultGoolDiv.textContent = `Vitoria do ${player.nameTime} com ${gool} Gools.`
           }
         }
+
         playerCard.appendChild(nameTime)
         playerCard.appendChild(img)
         playerCard.appendChild(labelGool)
@@ -56,4 +60,75 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     })
     .catch((error) => console.error("Erro ao carregar os dados:", error))
+})**/
+
+players = [
+  {
+    nameTime: "SÃO PAULO",
+    image: "img/saopaulo.png",
+    gool: 0,
+  },
+  {
+    nameTime: "CORINTHIANS",
+    image: "img/corinthinas.png",
+    gool: 0,
+  },
+]
+
+const lista = document.querySelector(".catalog")
+
+players.forEach((item) => {
+  const playerCard = document.createElement("div")
+  playerCard.classList.add("team")
+
+  const nameTime = document.createElement("h2")
+  nameTime.textContent = item.nameTime
+
+  const img = document.createElement("img")
+  img.src = item.image
+  img.alt = item.nameTime
+
+  const labelGool = document.createElement("label")
+  labelGool.classList.add("teamGool")
+  labelGool.textContent = `Gools`
+
+  const inputGool = document.createElement("input")
+  inputGool.type = "text"
+  inputGool.classList.add("input")
+  inputGool.placeholder = item.gool
+  inputGool.id = "gool"
+
+  const divResult = document.createElement("div")
+  divResult.id = "result"
+
+  const divResultGool = document.createElement("div")
+  divResultGool.id = "resultGool"
+
+  const button = document.createElement("button")
+  button.type = "button"
+  button.textContent = "Vitória"
+  button.id = "button"
+  button.onclick = function vote() {
+    const resultDiv = document.getElementById("result")
+    resultDiv.innerHTML = " "
+    const resultGoolDiv = document.getElementById("resultGool")
+    resultGoolDiv.innerHTML = " "
+    var gool = document.getElementById("gool").value
+    gool.innerHTML = " "
+    if (gool == 0) {
+      resultDiv.textContent = `Vitória do ${item.nameTime}`
+    } else {
+      resultGoolDiv.textContent = `Vitoria do ${item.nameTime} com ${gool} Gools.`
+    }
+  }
+
+  playerCard.appendChild(nameTime)
+  playerCard.appendChild(img)
+  playerCard.appendChild(labelGool)
+  playerCard.appendChild(inputGool)
+  playerCard.appendChild(button)
+  playerCard.appendChild(divResult)
+  playerCard.appendChild(divResultGool)
+
+  lista.appendChild(playerCard)
 })
